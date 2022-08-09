3f555faf93a899280f65c175cc2d5203cd99a1df
do-it-fast <94513554+do-it-fast@users.noreply.github.com>
2022-08-09 23:34:40 +0530

    Add files via upload

---
+++ Q4/Shopping_Q4/src/haneeshQ4/Customer.java
@@ -0,0 +1,17 @@
+package haneeshQ4;
+
+public class Customer {
+	String cid,username,pass;
+	int phone;
+	
+	public Customer(String username,String pass,String cid,int phone) {
+		this.cid = cid;
+		this.phone = phone;
+		this.pass = pass;
+		this.username = username;
+	}
+	
+	public String toString() {
+		return "Username :"+username+"  CID: "+cid+"   PHONE: "+phone;
+	}
+}
---
+++ Q4/Shopping_Q4/src/haneeshQ4/Staff.java
@@ -0,0 +1,9 @@
+package haneeshQ4;
+
+public class Staff {
+	String username,password;
+	public Staff(String u,String p) {
+		this.username = u;
+		this.password = p;
+	}
+}
